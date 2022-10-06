1095b815355de79c965ae9b27ed67da2979d0334
hephzibahcentre <114697769+hephzibahcentre@users.noreply.github.com>
2022-10-06 10:20:27 +0100

    Github Desktop commit

---
+++ formWithValidation/UserControl1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace formWithValidation
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtConfirm = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.confirmBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(170, 43);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(134, 20);
+            this.txtUsername.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(170, 80);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(134, 20);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // txtConfirm
+            // 
+            this.txtConfirm.Location = new System.Drawing.Point(170, 115);
+            this.txtConfirm.Name = "txtConfirm";
+            this.txtConfirm.Size = new System.Drawing.Size(134, 20);
+            this.txtConfirm.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(109, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(111, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(73, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Confirm Password";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(170, 152);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(134, 23);
+            this.btnSubmit.TabIndex = 6;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // confirmBox
+            // 
+            this.confirmBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.confirmBox.ForeColor = System.Drawing.Color.Red;
+            this.confirmBox.Location = new System.Drawing.Point(76, 181);
+            this.confirmBox.Multiline = true;
+            this.confirmBox.Name = "confirmBox";
+            this.confirmBox.ReadOnly = true;
+            this.confirmBox.Size = new System.Drawing.Size(228, 49);
+            this.confirmBox.TabIndex = 7;
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.confirmBox);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtConfirm);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(370, 245);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtConfirm;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox confirmBox;
+    }
+}
---
+++ formWithValidation/UserControl1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formWithValidation
+{
+    public partial class UserControl1: UserControl
+    {
+        string userName = string.Empty;
+        string userPassword = string.Empty;
+        string userPwConfirm = string.Empty;
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            confirmBox.Text = String.Empty;
+
+            if (txtUsername.Text.Length < 3) {
+                confirmBox.AppendText("Username needs to be at least 3 character!");
+                confirmBox.AppendText(Environment.NewLine);
+            } else
+            {
+                userName = txtUsername.Text;
+            }
+
+            if (txtPassword.Text.Length < 6)
+            {
+                confirmBox.AppendText("Password needs to be at least 6 characters!");
+                confirmBox.AppendText(Environment.NewLine);
+            }
+            else
+            {
+                userPassword = txtPassword.Text;
+                if (txtConfirm.Text != txtPassword.Text)
+                {
+                    confirmBox.AppendText("Passwords do not match!");
+                }
+                else
+                {
+                    userPwConfirm = txtConfirm.Text;
+                }
+            }
+
+            if (userName.Length > 1 && userPwConfirm.Length > 1)
+            {
+                confirmBox.AppendText("Registered!");
+                this.BackColor = System.Drawing.Color.Purple;
+            }
+
+        }
+    }
+}
